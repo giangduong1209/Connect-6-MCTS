f0d5d2148b839a175bf11f3636b5d94b520bf139
giangduong1209 <giangduong120900@gmail.com>
2021-06-13 20:08:36 +0700

    Add new file state

---
+++ state.py
@@ -0,0 +1,72 @@
+import numpy as np
+class Connect6State():
+    def game_result(self):
+        # check if game is over
+        rowsum = np.sum(self.board, 0)
+        colsum = np.sum(self.board, 1)
+        diag_sum_tl = self.board.trace()
+        diag_sum_tr = self.board[::-1].trace()
+
+        player_one_wins = any(rowsum == self.board_size)
+        player_one_wins += any(colsum == self.board_size)
+        player_one_wins += (diag_sum_tl == self.board_size)
+        player_one_wins += (diag_sum_tr == self.board_size)
+
+        if player_one_wins:
+            return self.x
+
+        player_two_wins = any(rowsum == -self.board_size)
+        player_two_wins += any(colsum == -self.board_size)
+        player_two_wins += (diag_sum_tl == -self.board_size)
+        player_two_wins += (diag_sum_tr == -self.board_size)
+
+        if player_two_wins:
+            return self.o
+
+        if np.all(self.board != 0):
+            return 0.
+
+        # if not over - no result
+        return None
+
+    def is_game_over(self):
+        return self.game_result is not None
+
+    def is_move_legal(self, move):
+        # check if correct player moves
+        if move.value != self.next_to_move:
+            return False
+
+        # check if inside the board on x-axis
+        x_in_range = (0 <= move.x_coordinate < self.board_size)
+        if not x_in_range:
+            return False
+
+        # check if inside the board on y-axis
+        y_in_range = (0 <= move.y_coordinate < self.board_size)
+        if not y_in_range:
+            return False
+
+        # finally check if board field not occupied yet
+        return self.board[move.x_coordinate, move.y_coordinate] == 0
+
+    def move(self, move):
+        if not self.is_move_legal(move):
+            raise ValueError(
+                "move {0} on board {1} is not legal". format(move, self.board)
+            )
+        new_board = np.copy(self.board)
+        new_board[move.x_coordinate, move.y_coordinate] = move.value
+        if self.next_to_move == Connect6State.x:
+            next_to_move = Connect6State.o
+        else:
+            next_to_move = Connect6State.x
+
+        return Connect6State(new_board, next_to_move)
+
+    def get_legal_actions(self):
+        indices = np.where(self.board == 0)
+        return [
+            Connect6State(coords[0], coords[1], self.next_to_move)
+            for coords in list(zip(indices[0], indices[1]))
+        ]

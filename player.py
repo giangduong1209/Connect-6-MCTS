1c60763afa8405a5d7c65da678b75db232bf48be
giangduong1209 <giangduong120900@gmail.com>
2021-06-15 14:37:07 +0700

    Add new file

---
+++ player.py
@@ -0,0 +1,22 @@
+from board import SIZE, B, W, Empty, Edge
+import random
+import mcts
+class Player:
+    def __init__(self, stone):
+        self.stone = stone
+        self.opponent = - stone
+
+    def firstMove(self):
+        return (10, 10)
+
+    def move(self, board):
+        raise NotImplementedError
+
+class RandomPlayer(Player):
+    def collectEmpty(self, board):
+        return [(i, j) for j in range (SIZE) for i in range(SIZE) if board[i][j] == Empty]
+
+    def move(self, board):
+        empty = self.collectEmpty(board)
+        candidates = random.sample(empty, 2)
+        return candidates

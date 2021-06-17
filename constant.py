de6f579782b3902c9043b7383915db9009681de0
giangduong1209 <giangduong120900@gmail.com>
2021-06-17 13:02:52 +0700

    Change obj

--- constant.py
+++ constant.py
@@ -2,7 +2,7 @@ import numpy as np
 class Constant:
     def __init__(self, player):
         self.player = player
-    def Opponent(self, player):
+    def Opponent(self):
         if(self.player == 1):
             self.player = 0 
         else:

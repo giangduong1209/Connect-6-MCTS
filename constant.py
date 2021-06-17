a6f7045fe3a2efe66cb6614ea1e64953a3c33d36
giangduong1209 <giangduong120900@gmail.com>
2021-06-17 10:55:42 +0700

    Set player

---
+++ constant.py
@@ -0,0 +1,10 @@
+import numpy as np 
+class Constant:
+    def __init__(self, player):
+        self.player = player
+    def Opponent(self, player):
+        if(self.player == 1):
+            self.player = 0 
+        else:
+            self.player = 1 
+        return self.player

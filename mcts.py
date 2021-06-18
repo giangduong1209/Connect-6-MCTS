dac31d8938483c730f7af63c53f80213e182e69c
giangduong1209 <giangduong120900@gmail.com>
2021-06-18 20:20:15 +0700

    Del _

--- mcts.py
+++ mcts.py
@@ -8,16 +8,16 @@ class MonteCarloTreeSearch(object):
             assert(total_simulation_seconds is not None)
             end_time = time.time() + total_simulation_seconds
             while time.time() < end_time:
-                v = self._tree_policy()
+                v = self.tree_policy()
                 reward = v.rollout()
                 v.backpropagate(reward)
         else :
             for _ in range(0, simulations_number):            
-                v = self._tree_policy()
+                v = self.tree_policy()
                 reward = v.rollout()
                 v.backpropagate(reward)
         return self.root.best_child(c_param=0.)
-    def _tree_policy(self):
+    def tree_policy(self):
         current_node = self.root
         while not current_node.is_terminal_node():
             if not current_node.is_fully_expanded():

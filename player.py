6f6ac34b6927665cc234e620045ba76d13a6c57c
giangduong1209 <giangduong120900@gmail.com>
2021-06-19 12:41:30 +0700

    Add humanplayer

--- player.py
+++ player.py
@@ -1,24 +1,31 @@
-from board import SIZE, B, W, Empty, Edge
+from constant import SIZE, B, W, EMPTY, EDGE
 import random
-# from mcts import MCTS
+import mcts
 class Player:
-    def __init__(self, stone):
-        self.stone = stone
-        self.opponent = - stone
+	def __init__(self, stone):
+		self.stone = stone
+		self.opponent = - stone
 
-    def firstMove(self):
-        return (10, 10)
+	def first_move(self):
+		return (10, 10)
 
-    def move(self, board):
-        raise NotImplementedError
-
-# mcts = MCTS()
+	def move(self, board):
+		raise NotImplementedError
+		
+	def collectEmpty(self, board):
+		return [(i, j) for j in range (SIZE) for i in range(SIZE) if board[i][j] == EMPTY]
 
 class RandomPlayer(Player):
-    def collectEmpty(self, board):
-        return [(i, j) for j in range (SIZE) for i in range(SIZE) if board[i][j] == Empty]
-
-    def move(self, board):
-        empty = self.collectEmpty(board)
-        candidates = random.sample(empty, 2)
-        return candidates
+	def move(self, board):
+		empty = self.collectEmpty(board)
+		print(empty)
+		candidates = random.sample(empty, 2)
+		print(candidates)
+		return candidates
+		
+class HumanPlayer(Player):
+	def move(self, board):
+		empty = self.collectEmpty(board)
+		print(empty)
+		candidates = ([int(input("Row 1:")),int(input("Column 1:"))], [int(input("Row 2:")),int(input("Column 2:"))])
+		return candidates
